0be90640d7a9abd9893bec161e2cfc292f1f29ee
pebritisnasetiawan <pebritisnasetiawan@gmail.com>
2024-10-15 23:23:57 +0700

    delete product from order

--- src/views/cart/IndexView.vue
+++ src/views/cart/IndexView.vue
@@ -2,7 +2,12 @@
   <div class="">
     <div id="page-wrap">
       <h1>Shopping Cart</h1>
-      <ItemCart v-for="item in cartItems" :key="item.id" :item="item" />
+      <ItemCart
+        v-for="item in cartItems"
+        :key="item.id"
+        :item="item"
+        v-on:remove-item="removeFromCart($event)"
+      />
       <h3 id="total-price">Total: {{ totalPrice }}</h3>
       <button id="checkout-button">Checkout</button>
     </div>
@@ -25,6 +30,19 @@ export default {
   computed: {
     totalPrice() {
       return this.cartItems.reduce((sum, item) => sum + Number(item.price), 0);
+    },
+  },
+  methods: {
+    async removeFromCart(product) {
+      await axios.delete(
+        `http://localhost:8000/api/orders/delete/user/1/product/${product}`
+      );
+      let indexCart = this.cartItems
+        .map(function (item) {
+          return item.code;
+        })
+        .indexOf(product);
+      this.cartItems.splice(indexCart, 1);
     },
   },
   async created() {
