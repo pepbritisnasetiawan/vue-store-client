a7f2f628de82e12f372c6094b5ed2a4db12708f3
pebritisnasetiawan <pebritisnasetiawan@gmail.com>
2024-10-15 17:29:51 +0700

    add: reusable component and props

--- src/views/product/IndexView.vue
+++ src/views/product/IndexView.vue
@@ -2,25 +2,20 @@
   <div class="">
     <div class="" id="page-wrap">
       <div class="grid-wrap">
-        <div class="product-item" v-for="product in products" :key="product.id">
-          <img :src="product.imageUrl" alt="" />
-          <h3>{{ product.name }}</h3>
-          <p class="product-price">Rp{{ product.price }}</p>
-          <router-link
-            :to="{ name: 'product-detail', params: { id: product.id } }"
-          >
-            <button>Detail</button></router-link
-          >
-        </div>
+        <ProductItem v-for="product in products" :key="product.id" :product="product"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ProductItem from '@/components/ProductItem.vue';
 import { products } from '@/data-seed';
 
 export default {
+  components: {
+    ProductItem
+  },
   data() {
     return {
       products,
@@ -37,33 +32,5 @@ export default {
   margin-top: 16px;
 }
 
-.product-item {
-  align-items: center;
-  border-radius: 8px;
-  box-shadow: 0px 1px 2px #888;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 2%;
-  padding: 20px;
-  position: relative;
-  width: 32%;
-}
 
-.product-name {
-  margin-bottom: 0;
-}
-
-img {
-  height: 200px;
-  width: 200px;
-  border-radius: 5%;
-}
-
-a {
-  width: 100%;
-}
-
-button {
-  width: 100%;
-}
 </style>
